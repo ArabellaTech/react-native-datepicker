5d16c899b504b8773f336e36796a6b328034b89e
Piotr Grzelczak <piotr.grzelczak@gmail.com>
2017-01-24 16:33:54 +0100

    update date format

--- index.js
+++ index.js
@@ -122,12 +122,14 @@ class DatePicker extends Component {
   getDateStr(date = this.props.date) {
     const {mode, format = FORMATS[mode]} = this.props;
 
+    console.log('moment(date).isValid()', moment(date).isValid())
     if (date instanceof Date) {
       console.log('is instanceof date date', date)
       console.log('is instanceof date Moment(date).format()', Moment(date).format())
       return Moment(date).format();
     } else {
       console.log('is not instanceof date date', date)
+      console.log('this.getDate(date)', this.getDate(date))
       console.log('is not instanceof date Moment(date).format()', Moment(this.getDate(date)).format())
       return Moment(this.getDate(date)).format();
     }
